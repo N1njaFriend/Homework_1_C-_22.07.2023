578c9231c51d98b708ec7873aa7d251cee8ecd8d
eN1nja <mr.buntar@gmail.com>
2023-07-22 21:32:05 +0300

    Добавил задачу 4

--- Задача_2/Program.cs
+++ Задача_2/Program.cs
@@ -96,7 +96,10 @@
 
 // Console.WriteLine(result);
 
-// Задача_2
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
 
 Console.WriteLine("Введите число A");
 int numA =Convert.ToInt32(Console.ReadLine());
@@ -150,5 +153,3 @@ else
 // {
 //     Console.Write("Ваше число C : "); Console.WriteLine(numC);
 // }
-
-// Задача 6
---
+++ Задача_4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число A");
+int numA =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int numB =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число C");
+int numC =Convert.ToInt32(Console.ReadLine());
+
+if(numA>numB)
+{
+    Console.Write("Ваше число A : "); Console.WriteLine(numA);
+}
+else if(numB>numC)
+{
+    Console.Write("Ваше число B : "); Console.WriteLine(numB);
+}
+else if(numC>numA)
+{
+    Console.Write("Ваше число C : "); Console.WriteLine(numC);
+}
+else if(numA>numC)
+{
+    Console.Write("Ваше число A : "); Console.WriteLine(numA);
+}
+else if(numB>numA)
+{
+    Console.Write("Ваше число B : "); Console.WriteLine(numB);
+}
+else if(numC>numB)
+{
+    Console.Write("Ваше число C : "); Console.WriteLine(numC);
+}
