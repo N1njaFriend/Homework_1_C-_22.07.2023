1122e64d13d047e833186388d3bde3c21753a3cd
eN1nja <mr.buntar@gmail.com>
2023-07-22 22:34:11 +0300

    решил задачу 8 и убрал подсказки в задаче 2

---
+++ Задача_8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число : ");
+int num =Convert.ToInt32(Console.ReadLine());
+
+int newNeg=num*0;
+
+while(newNeg<=num)
+{
+    Console.Write("Ваш результат : "); Console.WriteLine(newNeg);
+    newNeg=newNeg+2;
+}
